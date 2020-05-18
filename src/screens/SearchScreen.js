5291cc57f984e1232f9dde0a7e6799944a03ff9f
Claire <claire.cy.t@gmail.com>
2020-05-17 21:51:34 -0400

    Display results detail with image

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -12,23 +12,21 @@ const SearchScreen = () => {
     const filterResultsByPrice = (price) => {
         return results.filter(result => {
             return result.price ===  price;
-        })
+        });
     };
 
-    console.log(results);
 
 
-    return <View>
+    return <>
         <SearchBar term = {term} onTermChange={newTerm => setTerm(newTerm)}
         onTermSubmit = {()=>searchApi(term)}/>
         {errorMessage? <Text>{errorMessage}</Text>: null}
-        <Text>We have found {results.length} result</Text>
         <ScrollView>
-        <ResultsList results={filterResultsByPrice('$')}title = "Cost Effective"/>
-        <ResultsList results={filterResultsByPrice('$$')}title = "Bit Pricier"/>
-        <ResultsList results={filterResultsByPrice('$$$')}title = "Big Spender"/>
+        <ResultsList results={filterResultsByPrice('$')} title = "Cost Effective" />
+        <ResultsList results={filterResultsByPrice('$$')} title = "Bit Pricier" />
+        <ResultsList results={filterResultsByPrice('$$$')} title = "Big Spender" />
         </ScrollView>
-    </View>
+    </>
 }
 
 const styles = StyleSheet.create({});
